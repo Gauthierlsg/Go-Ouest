9fb006df20f9dd33ba926386a3071fa7d7bf6a20
Gotié <gauthier.lesage2@gmail.com>
2026-03-18 12:27:48 +0100

    fix: confettis uniquement quand le podium est complet

--- src/views/podium.js
+++ src/views/podium.js
@@ -163,6 +163,7 @@ export function renderPodium(container) {
     </div>`;
   }).join('');
 
+  const podiumComplete = podium.every(({ duo }) => duo?.team);
   let confetti = [];
   let animId;
   let t = 0;
@@ -170,7 +171,7 @@ export function renderPodium(container) {
   function resize() {
     canvas.width  = canvas.offsetWidth;
     canvas.height = canvas.offsetHeight;
-    confetti = mkConfetti(canvas.width);
+    if (podiumComplete) confetti = mkConfetti(canvas.width);
   }
 
   function tick() {
